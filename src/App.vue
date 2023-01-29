af2032d5573856abdfe36c57520355a6862519e9
workmates@github.io <workmates.live@outlook.com>
2023-01-28 18:48:44 -0600

    sa

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,20 @@
         </div>
       </div>
     </section>
+    <section class="section-container" id="pilots" style="width:894px; height:714px;">
+      <div style="height:52px; overflow:hidden;">
+        <div class="section-header clipped-medium-backward-pilot">
+          <img src="/icons/pilot-icon.svg" />
+          <h1>Pilot Roster</h1>
+        </div>
+        <div class="rhombus-back">&nbsp;</div>
+      </div>
+      <div class="section-content-container">
+        <div class="pilot-list-container">
+          <Pilot v-for="item in this.pilots" :key="item.slug" :pilot="item" />
+        </div>
+      </div>
+    </section>
   </div>
   <svg
     style="visibility: hidden; position: absolute;"
