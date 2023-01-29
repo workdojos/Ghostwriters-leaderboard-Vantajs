da9c2ada7cd8e08361021750ac0cefd1fc1f32ee
workmates@github.io <workmates.live@outlook.com>
2023-01-28 18:47:24 -0600

    as

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
   <Header :header="this.header" />
   <div class="content-container">
-    <section class="section-container" id="events" style="width:435px; height:714px;">
+    <section class="section-container" id="missions" style="width:435px; height:714px;">
       <div class="section-header clipped-medium-backward">
-        <img src="/icons/events-icon.svg" />
-        <h1>Events Log</h1>
+        <img src="/icons/mission-icon.svg" />
+        <h1>Background</h1>
       </div>
       <div class="section-content-container">
-        <Markdown :source="events" class="markdown" />
-      </div>
-    </section>
-    <section class="section-container" id="pilots" style="width:894px; height:714px;">
-      <div style="height:52px; overflow:hidden;">
-        <div class="section-header clipped-medium-backward-pilot">
-          <img src="/icons/pilot-icon.svg" />
-          <h1>Pilot Roster</h1>
-        </div>
-        <div class="rhombus-back">&nbsp;</div>
-      </div>
-      <div class="section-content-container">
-        <div class="pilot-list-container">
-          <Pilot v-for="item in this.pilots" :key="item.slug" :pilot="item" />
+        <h3>Current Assignment</h3>
+        <Markdown :source="current_md" class="markdown" />
+        <h3>Mission List</h3>
+        <div class="mission-list-container">
+          <Mission
+            v-for="item in this.missions"
+            :key="item.slug"
+            :mission="item"
+            :selected="this.mission_slug"
+            @click="selectMission(item)"
+          />
         </div>
       </div>
     </section>
