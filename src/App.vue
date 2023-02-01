11b8fd04456d2aaa17beb304462311aa4e5f6c77
workmates@github.io <workmates.live@outlook.com>
2023-01-31 18:17:00 -0600

    as

--- src/App.vue
+++ src/App.vue
@@ -92,37 +92,41 @@ export default {
       ],
       "pilots": [
         {
-          "callsign": "Jack",
-          "alias": "Wild Jack",
-          "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "4x4s"
-        },
-        {
-          "callsign": "Kal",
-          "alias": "Vier Flau",
-          "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Projects"
-        },
-        {
-          "callsign": "Starboard",
-          "alias": "Maria",
-          "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Marketing"
-        },
-        {
-          "callsign": "Maria",
-          "alias": "Maria Gomez",
+          "callsign": "Alex",
+          "alias": "Alex Theodorus",
           "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Science"
+          "corpro": "Available",
+          "frame": "Outdoor ",
+          "mech": "Explorer"
         },
+
+        {
+          "callsign": "Akiko",
+          "alias": "Akiko Tanaka",
+          "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
+          "corpro": "Available",
+          "frame": "Complementary And Alternative Medicine ",
+          "mech": "Healer"
+        },
+
+        {
+          "callsign": "Adia",
+          "alias": "Adia Ndume",
+          "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
+          "corpro": "Available",
+          "frame": "Physical Therapy ",
+          "mech": "Physical Therapist"
+        },
+
+        {
+          "callsign": "Adalynn",
+          "alias": "Adalynn Chantelle",
+          "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
+          "corpro": "Available",
+          "frame": "Perfoming Arts ",
+          "mech": "Playwright"
+        },
+
       ],
       "header": {
         "planet": "Hercynia",
