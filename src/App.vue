054378d88b16e1505dd3e74aecc632ca5cbed046
workmates@github.io <workmates.live@outlook.com>
2023-02-02 11:48:07 -0600

    sassy

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@
       <div style="height:52px; overflow:hidden;">
         <div class="section-header clipped-medium-backward-pilot">
           <img src="/icons/pilot-icon.svg" />
-          <h1>Pilot Roster</h1>
+          <h1>Roster</h1>
         </div>
         <div class="rhombus-back">&nbsp;</div>
       </div>
@@ -1564,7 +1564,7 @@ export default {
         "headerTitle": "WORKDOJOS",
         "headerSubtitle": "COMPENDIUM",
         "subheaderTitle": "BUILT WITH VANTA.JS",
-        "subheaderSubtitle": "MISSIONS & CO-PILOTS",
+        "subheaderSubtitle": "MISSION BRIEF",
       },
       "options":{
         "eventsMarkdownPerMission": true
