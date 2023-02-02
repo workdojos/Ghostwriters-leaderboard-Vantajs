9367701ffffe57574149f6a09cefde7d197860c2
workmates@github.io <workmates.live@outlook.com>
2023-02-01 19:12:11 -0600

    sads

--- src/App.vue
+++ src/App.vue
@@ -1353,7 +1353,7 @@ export default {
 
         {
           "callsign": "Susanna",
-          "alias": "Susanna Smith",
+          "alias": "Samantha Smith",
           "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
           "corpro": "Available",
           "frame": "Finance ",
