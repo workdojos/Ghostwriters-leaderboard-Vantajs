a550f6177f4536254a007de744f5dd7be11e0c38
workmates@github.io <workmates.live@outlook.com>
2023-02-03 10:54:52 -0600

    asa

--- src/components/Mission.vue
+++ src/components/Mission.vue
@@ -30,7 +30,7 @@ export default {
 			return `/icons/mission-${this.mission.status}.svg`
 		},
 		missionStatus() {
-			if (this.mission.status === "start") return "Current\nBriefing"
+			if (this.mission.status === "start") return "\n"
 			if (this.mission.status === "partial-success") return "Partial\nSuccess"
 			if (this.mission.status === "success") return "Mission\nSuccess"
 			if (this.mission.status === "failure") return "Mission\nFailure"
